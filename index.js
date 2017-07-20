a1919cc99e016fa922790e56e5a45cd70d210bbf
Ahmed <destiny_hacker@hotmail.com>
2017-07-20 20:47:46 +0200

    finished html+css, added js input validation
    - added colors and fonts
    - removed HTML required attribute, using bootstrap validator instead.
    - removed save as PDF button, jsPDF doesn't work on any mobile device,
    will re-add this feature if it gets fixed.
    - fixed select menu arrow cross-browser styling.
    - added input validation
    - minor CSS changes here and there ( i'm too lazy for this ).

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
+
 $(document).ready(function () {
-    $("#btnSave").click(function() { 
-        html2canvas($("#solution"), {
-            onrendered: function(canvas) {
-                theCanvas = canvas;
-                var data = canvas.toDataURL('image/png');
-                var image = new Image();
-                image.src = data;
-                var doc = new jsPDF();
-                doc.addImage(image,0,0,0)
-                doc.save('solution.pdf')
-                }
-        });
-    });
+    //define sections object here (and bolts properties maybe?)
     
+    //append bolt-type select menu when "bolted" connection is picked
     $("#bolted").click(function() {
-        if ($("#bolt-label").length == 0) {
-        var selectMenu = '<label for="bolt" id="bolt-label">Bolt:</label><select id="bolt" required><option value="M12" selected>M12</option><option value="M14">M14</option><option value="M16">M16</option></select>';
+        if ($("#bolt-label").length === 0) {
+        var selectMenu = '<label for="bolt" id="bolt-label">Bolt:</label><select class="form-control" id="bolt" required><option value="M12" selected>M12</option><option value="M14">M14</option><option value="M16">M16</option></select>';
         $(selectMenu).appendTo($("#connection"));
         }
     });
+    //removes bolt-type select menu when "welded" connection is picked (if it exists)
     $("#welded").click(function() {
         $("#bolt").remove();
         $("#bolt-label").remove();
     });
+    
+    //store objects that require validation then enter input validation stage (style only)
+    update();
+    
+    //update array of objects that require validation when design procedure is changed  
+    $("#main-selector").change( function() {
+        update();
+    });
+    
+    //do a real check for values and then call the required design function
+    $("#btn-solve").click( function() {
+        $(".validate").each( function(i,el) {
+            if (!$(el).data("valid")) {
+                return null;
+            }
+            else {
+                //pick design function and call it
+            }
+        });
+    });
+    
+    
 });
+
+function checkMe() {
+    if (/\D+/.test($("#" + this.id).val()) === true || $("#" + this.id).val() <= 0) {
+        $("#" + this.id).css("border-color","#a94442");
+        $("label[for="+this.id+"]").css("color", "#a94442");
+        $(this).data("valid", false); 
+    }
+    else {
+        $("#" + this.id).css("border-color","#ccc");
+        $("label[for="+this.id+"]").css("color", "#333");
+        $(this).data("valid", true);
+        
+    }
+}
+
+function update() {
+    var validateArr = $(".validate");
+    validateArr.each( function(i,el) {
+        $(el).bind("change", checkMe);
+    });
+    //INSERT HTML-APPENDING INPUT-UPDATING CODE HERE!!
+}
