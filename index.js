0fea1e3448b02bd5a70fd8613f6e4d25db44b664
Ahmed <destiny_hacker@hotmail.com>
2017-07-22 04:57:50 +0200

    finished tension design function
    - finished tension design function.
    
    the webapp should be able to solve tension problems now, will share it
    with a few people to test everything out

--- index.js
+++ index.js
@@ -1,10 +1,78 @@
+var sections = [{"angle":"30*3","area":1.74,"e":0.84,"Iy":1.41,"ix":0.9},
+{"angle":"35*3","area":2.04,"e":0.36,"Iy":2.29,"ix":1.06},
+{"angle":"30*4","area":2.27,"e":0.89,"Iy":1.81,"ix":0.89},
+{"angle":"35*4","area":2.67,"e":1,"Iy":2.96,"ix":1.05},
+{"angle":"30*5","area":2.78,"e":0.92,"Iy":2.16,"ix":0.88},
+{"angle":"40*4","area":3.08,"e":1.12,"Iy":4.48,"ix":1.21},
+{"angle":"35*5","area":3.28,"e":1.04,"Iy":0.56,"ix":1.04},
+{"angle":"40*5","area":3.79,"e":1.16,"Iy":5.43,"ix":1.2},
+{"angle":"45*5","area":4.3,"e":1.28,"Iy":7.83,"ix":1.35},
+{"angle":"40*6","area":4.48,"e":1.2,"Iy":6.33,"ix":1.19},
+{"angle":"50*5","area":4.8,"e":1.4,"Iy":11,"ix":1.51},
+{"angle":"45*6","area":5.09,"e":1.32,"Iy":9.16,"ix":1.34},
+{"angle":"55*5","area":5.32,"e":1.52,"Iy":14.7,"ix":1.66},
+{"angle":"50*6","area":5.69,"e":1.45,"Iy":12.8,"ix":1.5},
+{"angle":"45*7","area":5.86,"e":1.36,"Iy":10.4,"ix":1.33},
+{"angle":"55*6","area":6.31,"e":1.56,"Iy":17.8,"ix":1.66},
+{"angle":"50*7","area":6.56,"e":1.49,"Iy":14.6,"ix":1.49},
+{"angle":"60*6","area":6.91,"e":1.69,"Iy":22.8,"ix":1.82},
+{"angle":"55*8","area":8.23,"e":1.64,"Iy":22.1,"ix":1.64},
+{"angle":"65*7","area":8.7,"e":1.85,"Iy":33.4,"ix":1.96},
+{"angle":"60*8","area":9.03,"e":1.77,"Iy":29.1,"ix":1.8},
+{"angle":"70*7","area":9.4,"e":1.97,"Iy":42.4,"ix":2.12},
+{"angle":"65*8","area":9.85,"e":1.89,"Iy":37.5,"ix":1.95},
+{"angle":"75*7","area":10.1,"e":2.03,"Iy":52.4,"ix":2.28},
+{"angle":"65*9","area":11,"e":1.93,"Iy":41.3,"ix":1.94},
+{"angle":"60*10","area":11.1,"e":1.85,"Iy":34.9,"ix":1.78},
+{"angle":"75*8","area":11.5,"e":2.13,"Iy":58.9,"ix":2.26},
+{"angle":"70*9","area":11.9,"e":2.05,"Iy":52.6,"ix":2.1},
+{"angle":"80*8","area":12.3,"e":2.26,"Iy":72.3,"ix":2.42},
+{"angle":"75*10","area":14.1,"e":2.21,"Iy":71.4,"ix":2.25},
+{"angle":"70*11","area":14.3,"e":2.13,"Iy":61.8,"ix":2.09},
+{"angle":"80*10","area":15.1,"e":2.34,"Iy":87.5,"ix":2.41},
+{"angle":"90*9","area":15.5,"e":2.54,"Iy":116,"ix":2.74},
+{"angle":"80*12","area":17.9,"e":2.41,"Iy":102,"ix":2.39},
+{"angle":"90*11","area":18.2,"e":2.62,"Iy":138,"ix":2.2},
+{"angle":"100*10","area":19.2,"e":2.82,"Iy":177,"ix":3.04},
+{"angle":"110*10","area":21.2,"e":3.07,"Iy":239,"ix":3.36},
+{"angle":"90*13","area":21.8,"e":2.7,"Iy":158,"ix":2.69},
+{"angle":"100*12","area":22.7,"e":2.9,"Iy":207,"ix":3.02},
+{"angle":"110*12","area":25.1,"e":3.15,"Iy":280,"ix":3.34},
+{"angle":"100*14","area":26.2,"e":2.98,"Iy":295,"ix":3},
+{"angle":"120*12","area":27.5,"e":3.4,"Iy":368,"ix":3.65},
+{"angle":"110*14","area":29,"e":3.21,"Iy":319,"ix":3.82},
+{"angle":"120*13","area":29.9,"e":3.44,"Iy":394,"ix":6.64},
+{"angle":"130*12","area":30,"e":3.64,"Iy":472,"ix":3.97},
+{"angle":"120*15","area":33.9,"e":3.51,"Iy":446,"ix":3.63},
+{"angle":"130*14","area":34.7,"e":3.72,"Iy":540,"ix":3.94},
+{"angle":"140*13","area":35,"e":3.92,"Iy":638,"ix":4.27},
+{"angle":"130*16","area":39.3,"e":3.8,"Iy":605,"ix":3.92},
+{"angle":"140*15","area":40,"e":4,"Iy":723,"ix":2.25},
+{"angle":"150*14","area":40.3,"e":4.21,"Iy":845,"ix":4.58},
+{"angle":"150*15","area":43,"e":4.25,"Iy":898,"ix":4.57},
+{"angle":"150*16","area":45.7,"e":4.29,"Iy":949,"ix":4.56},
+{"angle":"160*15","area":46.1,"e":4.49,"Iy":1100,"ix":4.88},
+{"angle":"150*18","area":51,"e":4.36,"Iy":1050,"ix":4.54},
+{"angle":"160*17","area":51.8,"e":4.57,"Iy":1230,"ix":4.86},
+{"angle":"180*16","area":55.4,"e":4.02,"Iy":1680,"ix":5.51},
+{"angle":"150*20","area":56.3,"e":4.44,"Iy":1150,"ix":4.51},
+{"angle":"160*19","area":57.5,"e":4.65,"Iy":1350,"ix":4.84},
+{"angle":"200*16","area":61.8,"e":5.52,"Iy":1340,"ix":6.15},
+{"angle":"180*18","area":61.9,"e":4.1,"Iy":1870,"ix":5.49},
+{"angle":"180*20","area":68.4,"e":5.18,"Iy":2040,"ix":5.47},
+{"angle":"200*18","area":69.1,"e":5.6,"Iy":2600,"ix":6.13},
+{"angle":"180*22","area":74.7,"e":5.26,"Iy":2210,"ix":5.44},
+{"angle":"200*20","area":76.4,"e":5.68,"Iy":1850,"ix":6.11},
+{"angle":"200*24","area":90.6,"e":5.84,"Iy":3330,"ix":6.06},
+{"angle":"200*28","area":105,"e":5.99,"Iy":3780,"ix":6.02}]
+
 $(document).ready(function () {
-    //define sections object here (and bolts properties maybe?)
+    
     
     //append bolt-type select menu when "bolted" connection is picked
     $("#bolted").click(function() {
         if ($("#bolt-label").length === 0) {
-        var selectMenu = '<label for="bolt" id="bolt-label" class="control-label col-xs-1">Bolt:</label><div id="removeme" class="col-xs-2"><select class="form-control" id="bolt"><option value="12" selected>M12</option><option value="14">M14</option><option value="16">M16</option></select></div>';
+        var selectMenu = '<label for="bolt" id="bolt-label" class="control-label col-xs-1">Bolt:</label><div id="removeme" class="col-xs-2"><select class="form-control" id="bolt"><option value="1.2" selected>M12</option><option value="1.6">M16</option><option value="2.0">M20</option><option value="2.2">M22</option><option value="2.4">M24</option></select></div>';
         $(selectMenu).appendTo($("#connection"));
         }
     });
@@ -69,10 +137,10 @@ function update() {
 }
 
 function tension(df,sg,connection,bolts,shape,ca,lx,ly,tg) {
-    //inline $\sum_{i=0}^n i^2 = \frac{(n^2+n)(2n+1)}{6}$
+    //good luck trying to read this function
     var math = "<div>$A \\geqslant \\frac{"
     var value = ""
-    $("#solution-text").append("<p>A-Primary design:</p><p>1-stress:</p>");
+    $("#solution-text").append("<div>$\A-Primary\\hspace{0.2cm}design:-$</div><div>\\[\{1} - Stress:\\]</div>");
     math += "d.f.}{\\sigma_{t} \\times ";
     if (ca == 2) { math += "1.2 \\times "; }
     if (connection === "bolted") { math += "0.85 \\times "; }
@@ -86,10 +154,10 @@ function tension(df,sg,connection,bolts,shape,ca,lx,ly,tg) {
     if (shape === "one angle") { math += "0.6} = "; value += "*0.6)"; }
     else { math+= "2} = "; value += "*2)"; };
     var aStress = eval(value).toFixed(3);
-    math += aStress + " \\hspace{0.2cm} cm^2 \\hspace{0.5cm} \\sigma_{t}:Page \\hspace{0.2cm} 29$</div>";
+    math += aStress + " \\hspace{0.2cm} cm^2$</div>";
     $("#solution-text").append(math);
     
-    $("#solution-text").append("<p>2-stiffness:</p>");
+    $("#solution-text").append("<div>\\[\{2} - Stiffness:\\]</div>");
     math = "<div>$a \\geqslant \\frac{L_{x} \\times 100}{";
     value = lx*100;
     if (shape === "one angle") { math += "0.2 \\times 300}="; value = value/(0.2*300); }
@@ -116,11 +184,147 @@ function tension(df,sg,connection,bolts,shape,ca,lx,ly,tg) {
     math += aStiffness2 + "\\hspace{0.2cm} cm^2$</div>";
     $("#solution-text").append(math);
     
-    var aStiffness = aStiffness1 >= aStiffness2 ? aStiffness1:aStiffness2;
-    math = "<div>$\\therefore a=" + aStiffness + "\\hspace{0.7cm}" ;
-    //REQUIRES AREA TABLE
-    if (aStress >= aStiffness) { math += aStress + " \\geqslant " + aStiffness + "\\rightarrow use \\hspace{0.2cm} x*x*t$</div>"; }
-    else { math += aStiffness + "\\geqslant " + aStress + "\\rightarrow use \\hspace{0.2cm} x*x*t$</div>"; }
+    var aStiffness = parseFloat(aStiffness1) >= parseFloat(aStiffness2) ? parseFloat(aStiffness1):parseFloat(aStiffness2);
+    math = "<div>$\\therefore a=" + aStiffness + ",\\hspace{0.7cm}" ;
+    if (parseFloat(aStress) > parseFloat(aStiffness)) { math += "\\because" + aStress + " \\geqslant " + aStiffness + "\\rightarrow use \\hspace{0.2cm}"; var aPrimary = aStress;}
+    else { math += "\\because" + aStiffness + "\\geqslant " + aStress + "\\rightarrow use \\hspace{0.2cm}"; var aPrimary = aStiffness; }
+    var primarySection = findSection(aPrimary);
+    if ( primarySection === -1 ) { return -1; }
+    math += primarySection.angle + "$</div>";
+    $("#solution-text").append(math);
+    
+    math = "<div>$A=" + primarySection.area + "\\hspace{0.2cm}cm^2,\\hspace{0.2cm}e=" + primarySection.e + "\\hspace{0.2cm}cm,\\hspace{0.2cm} I_{y}=" + primarySection.Iy + "\\hspace{0.2cm}cm^4, \\hspace{0.2cm}i_{x}=" + primarySection.ix + "\\hspace{0.2cm}cm$</div>" ;
+    $("#solution-text").append(math);
+    
+    $("#solution-text").append("<div>$\B-Exact\\hspace{0.2cm}design:-$</div><div>\\[\{1} - Stiffness:\\]</div>");
+    math = "<div>$\\lambda_{x}=\\frac{L_{x} \\times 100}{" + primarySection.ix + "}=" +((lx*100)/parseFloat(primarySection.ix)).toFixed(3) + "\\leqslant  300 \\hspace{0.5cm}";
+    if ((lx*100)/parseFloat(primarySection.ix) <= 300) { math+= "O.K.$</div>"; }
+    else { 
+        while ((lx*100)/parseFloat(primarySection.ix) > 300) {
+            var rsec = refind(aPrimary); 
+            if (rsec === -1) { return -1; }
+            primarySection = rsec[0]; aPrimary = rsec[0].area ; math+= rsec[1];
+            $("#solution-text").append(math);
+            math = "<div>$\\lambda_{x}=\\frac{L_{x} \\times 100}{" + primarySection.ix + "}=" +((lx*100)/parseFloat(primarySection.ix)).toFixed(3) + "\\leqslant  300 \\hspace{0.5cm}";
+            if ((lx*100)/parseFloat(primarySection.ix) <= 300) { math+= "O.K.$</div>"; }
+        } 
+    } 
+     $("#solution-text").append(math);
+    
+    var iy = (Math.sqrt((parseFloat(primarySection.Iy)+parseFloat(primarySection.area)*Math.pow(parseFloat(primarySection.e) + (parseFloat(tg)/2), 2))/parseFloat(primarySection.area))).toFixed(3);
+    math = "<div>$i_{y}=\\sqrt{\\frac{I_{y}+A(e+\\frac{t_{g}}{2})^2}{A}}=\\sqrt{\\frac{" + primarySection.Iy + "+" + primarySection.area + "(" + primarySection.e + "+\\frac{" + tg + "}{2})^2}{" + primarySection.area + "}}=" + iy + "\\hspace{0.2cm}cm$</div>";
+    $("#solution-text").append(math);
+    
+    math = "<div>$\\lambda_{y}=\\frac{L_{y} \\times 100}{" + iy + "}=" +((ly*100)/parseFloat(iy)).toFixed(3) + "\\leqslant  300 \\hspace{0.5cm}";
+    if ((ly*100)/parseFloat(iy) <= 300) { math+= "O.K.$</div>"; }
+    else { 
+        while ((ly*100)/parseFloat(iy) > 300) {
+            var rsec = refind(aPrimary);
+            if (rsec === -1) { return -1; }
+            primarySection = rsec[0]; aPrimary = rsec[0].area ; math+= rsec[1];
+            $("#solution-text").append(math);
+            
+            iy = (Math.sqrt((parseFloat(primarySection.Iy)+parseFloat(primarySection.area)*Math.pow(parseFloat(primarySection.e) + (parseFloat(tg)/2), 2))/parseFloat(primarySection.area))).toFixed(3);
+            math = "<div>$i_{y}=\\sqrt{\\frac{" + primarySection.Iy + "+" + primarySection.area + "(" + primarySection.e + "+\\frac{" + tg + "}{2})^2}{" + primarySection.area + "}}=" + iy + "\\hspace{0.2cm}cm$</div>";
+    $("#solution-text").append(math);
+            math = "<div>$\\lambda_{y}=\\frac{L_{y} \\times 100}{" + iy + "}=" +((ly*100)/parseFloat(iy)).toFixed(3) + "\\leqslant  300 \\hspace{0.5cm}";
+            if ((ly*100)/parseFloat(iy) <= 300) { math+= "O.K.$</div>"; }
+        } 
+    }
+    $("#solution-text").append(math);
+    
+    $("#solution-text").append("<div>${2} - Stress:$</div>");
+    var aNet = 0;
+    if (connection === "welded") { 
+        if (shape === "one angle") { 
+            var a1 = ((parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10)*(parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10)).toFixed(3);
+            
+            var a2 = (((parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10)-(parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10))*(parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10)).toFixed(3); 
+            
+            aNet = (parseFloat(a1) + (3*parseFloat(a1) / (3*parseFloat(a1) + parseFloat(a2))) * parseFloat(a2)).toFixed(3);
+            
+            math = "<div>$\\because connection\\hspace{0.2cm}is\\hspace{0.2cm}welded\\hspace{0.2cm}and\\hspace{0.2cm}shape\\hspace{0.2cm}is\\hspace{0.2cm}one\\hspace{0.2cm}angle$</div><div>$ \\therefore A_{net}=A_{1}+(\\frac{3\\times A_{1}}{3 \\times A_{1} + A_{2}}) \\times A_{2}$</div><div>$A_{1}=a \\times t=" + (parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10) + "\\times" + (parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10) + "=" + a1 + "$</div><div>$A_{2}=(a-t)t=(" + (parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10) + "-" + (parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10) + ")" + (parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10) + "=" + a2 + "$</div><div>$\\therefore A_{net}=" + aNet + "$</div>"; }
+        
+        else {
+            aNet = 2 * parseFloat(primarySection.area);
+            math = "<div>$\\because\\hspace{0.2cm}connection\\hspace{0.2cm}is\\hspace{0.2cm}welded\\hspace{0.2cm}and\\hspace{0.2cm}shape\\hspace{0.2cm}is\\hspace{0.2cm}two\\hspace{0.2cm}angle\\hspace{0.2cm}or\\hspace{0.2cm}star$</div><div>$\\therefore A_{net}=2A=2\\times" + primarySection.area + "=" + aNet + "$</div>"; }
+        
+    }
+    else {
+        if( shape === "one angle" ) {
+            var a1 = ((parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10)*(parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10)-(parseFloat(bolts)+0.2)*(parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10)).toFixed(3);
+            var a2 = (((parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10)-(parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10))*(parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10)).toFixed(3);
+            aNet = (parseFloat(a1) + (3*parseFloat(a1) / (3*parseFloat(a1) + parseFloat(a2))) * parseFloat(a2)).toFixed(3);
+             math = "<div>$\\because connection\\hspace{0.2cm}is\\hspace{0.2cm}bolted\\hspace{0.2cm}and\\hspace{0.2cm}shape\\hspace{0.2cm}is\\hspace{0.2cm}one\\hspace{0.2cm}angle$</div><div>$\\therefore A_{net}=A_{1}+(\\frac{3\\times A_{1}}{3 \\times A_{1} + A_{2}}) \\times A_{2}$</div><div>$A_{1}=a \\times t - n (\\phi + 0.2)t="+ (parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10) + "\\times" + (parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10) + "-(" + (bolts) + "+0.2)" + (parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10) + "=" + a1 + "$</div><div>$A_{2}=(a-t)t=(" + (parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10) + "-" + (parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10) + ")" + (parseFloat(primarySection.angle.substr(0, primarySection.angle.indexOf("*")))/10) + "=" + a2 + "$</div><div>$\\therefore A_{net}=" + aNet + "$</div>"
+             }
+             
+        else {
+            aNet = (2*(parseFloat(primarySection.area)-(parseFloat(bolts)+0.2)*parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10)).toFixed(3);
+            math = "<div>$\\because connection\\hspace{0.2cm}is\\hspace{0.2cm}bolted\\hspace{0.2cm}and\\hspace{0.2cm}shape\\hspace{0.2cm}is\\hspace{0.2cm}two\\hspace{0.2cm}angle\\hspace{0.2cm}or\\hspace{0.2cm}star$</div><div>$\\therefore A_{net}=2[A-n(\\phi + 0.2)t]=2[" + primarySection.area + "-" + "(" + bolts + "+0.2)" + parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10 + "]=" + aNet + "$</div>";
+        }
+    }
+    math += "<div>$\\sigma_{t}";
+    if (sg == 37) { value = "1.4"; }
+    else if (sg == 44) { value = "1.6"; }
+    else { value = "2.1"; }
+    if (ca == 2) { math+= "\\times 1.2"; value+= "*1.2"}
+    math+= "\\geqslant \\frac{d.f.}{A_{net}} \\rightarrow";
+    if (sg == 37) { math += "1.4"; }
+    else if (sg == 44) { math += "1.6"; }
+    else { math += "2.1"; }
+    if (ca == 2) { math += "\\times 1.2"; }
+    math+= "\\geqslant \\frac{" + df + "}{" + aNet + "}\\hspace{0.5cm}";
+    if (eval(value) >= (parseFloat(df)/parseFloat(aNet))) { math += "O.K.$</div>"; }
+    else {
+        while (eval(value) < (parseFloat(df)/parseFloat(aNet))) {
+            var rsec = refind(aPrimary);
+            if (rsec === -1) { return -1; }
+            primarySection = rsec[0]; aPrimary = rsec[0].area ; math+= rsec[1];
+            $("#solution-text").append(math);
+            
+            aNet = (2*(parseFloat(primarySection.area)-(parseFloat(bolts)+0.2)*parseFloat(primarySection.angle.substr(primarySection.angle.indexOf("*")+1))/10)).toFixed(3);
+            math = "<div>$";
+            if (sg == 37) { math += "1.4"; }
+            else if (sg == 44) { math += "1.6"; }
+            else { math += "2.1"; }
+            if (ca == 2) { math+= "\\times 1.2";}
+            math+= "\\geqslant \\frac{" + df + "}{" + aNet + "}\\hspace{0.5cm}"
+            if (eval(value) >= (parseFloat(df)/parseFloat(aNet))) { math+= "O.K.$</div>"; }
+        } 
+    }
+    
     $("#solution-text").append(math);
     MathJax.Hub.Typeset();
+    
+}
+
+function findSection(a) {
+    //finds a proper section, returns -1 if none is found
+    for (var i=0 ; i<sections.length ; i++) {
+        if (sections[i].area >= a) { 
+            return sections[i];
+        }
+    }
+    $("#solution-text").append("<div>$unable\\hspace{0.2cm}to\\hspace{0.2cm}find\\hspace{0.2cm}a\\hspace{0.2cm}proper\\hspace{0.2cm}section.$</div>");
+    MathJax.Hub.Typeset();
+    return -1;
+}
+
+function refind(a) {
+    //tries to find a better section, returns -1 if none is found
+    for (var i=0 ; i<sections.length ; i++) {
+        if (sections[i].area >= a) {
+            try {
+                if (!sections[i+5].angle) { throw "sectionError"; };
+            }
+            catch (err) {
+              $("#solution-text").append("<div>$unable\\hspace{0.2cm}to\\hspace{0.2cm}find\\hspace{0.2cm}a\\hspace{0.2cm}proper\\hspace{0.2cm}section.$</div>");
+                MathJax.Hub.Typeset();
+                return -1;
+            }
+            var str = "Fails\\rightarrow use \\hspace{0.2cm}" + sections[i+5].angle + "$</div>";
+            str += "<div>$A=" + sections[i+5].area + "\\hspace{0.2cm}cm^2,\\hspace{0.2cm}e=" + sections[i+5].e + "\\hspace{0.2cm}cm,\\hspace{0.2cm} I_{y}=" + sections[i+5].Iy + "\\hspace{0.2cm}cm^4, \\hspace{0.2cm}i_{x}=" + sections[i+5].ix + "\\hspace{0.2cm}cm$</div>" ;
+            return [sections[i+5], str];
+        }
+    }
 }
